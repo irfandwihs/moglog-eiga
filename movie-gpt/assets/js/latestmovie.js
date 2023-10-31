bd80e893199b1c40d2306acd64332b004922d5c5
irfandwihs <irfandwi.hs@gmail.com>
2023-11-01 00:35:28 +0700

    add latest movie

---
+++ movie-gpt/assets/js/latestmovie.js
@@ -0,0 +1,81 @@
+const latestMovies = [
+  {
+    title: "Ballerina",
+    year: 2023,
+    rating: 6.2,
+    genre: "Action/Thriller",
+    genres: ["action", "thriller"],
+    country: "Korea",
+    countrys: ["korea"],
+    poster: "https://iili.io/JfwrRZN.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/n60OdZVDB58",
+    synopsis:
+      "Grieving the loss of a best friend she couldn't protect, an ex-bodyguard sets out to fulfill her dear friend's last wish: sweet revenge.",
+    duration: "1h 33m",
+  },
+  {
+    title: "Concrete Utopia",
+    year: 2023,
+    rating: 6.8,
+    genre: "Action/Adventure/Drama",
+    genres: ["action", "adventure", "drama"],
+    country: "Korea",
+    countrys: ["korea"],
+    poster: "https://iili.io/JfS8JcP.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/57CJngvMoqg",
+    synopsis:
+      "Survivors from a massive earthquake struggle for a new life in Seoul.",
+    duration: "2h 10m",
+  },
+  {
+    title: "Gran Turismo",
+    year: 2023,
+    rating: 7.2,
+    genre: "Action/Adventure/Drama",
+    genres: ["action", "adventure", "drama"],
+    country: "Inggris",
+    countrys: ["inggris"],
+    poster: "https://iili.io/JfSgisj.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/GgKmhDaVo48",
+    synopsis:
+      "Based on the unbelievable, inspiring true story of a team of underdogs - a struggling, working-class gamer, a failed former race car driver, and an idealistic motorsport exec - who risk it all to take on the most elite sport in the world.",
+    duration: "2h 14m",
+  },
+  {
+    title: "Nowhere",
+    year: 2023,
+    rating: 6.4,
+    genre: "Drama/Thriller",
+    genres: ["drama", "thriller"],
+    country: "Spanyol",
+    countrys: ["spanyol"],
+    poster: "https://iili.io/JfNnVOg.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/KDUtdcU10YA",
+    synopsis:
+      "A young pregnant woman named Mia escapes from a country at war by hiding in a maritime container aboard a cargo ship. After a violent storm, Mia gives birth to her child while lost at sea, where she must fight to survive.",
+    duration: "1h 49m",
+  },
+  {
+    title: "Poison",
+    year: 2023,
+    rating: 6.8,
+    genre: "Short/Comedy",
+    genres: ["short", "comedy"],
+    country: "USA",
+    countrys: ["usa"],
+    poster: "https://iili.io/JfS8WOu.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/FZVWaqKt81g",
+    synopsis:
+      "When a poisonous snake slithers onto an Englishman's stomach in India, his associate and a doctor race to save him.",
+    duration: "17m",
+  },
+  // Add more movies as desired
+];
+
+// Export the movies array to be used in other files
+export { latestMovies };
