16870c163b8bd394ef9a96577771527f73b3bb6e
irfandwihs <irfandwi.hs@gmail.com>
2023-11-17 23:40:31 +0700

    update

--- movie-gpt/assets/js/movie.js
+++ movie-gpt/assets/js/movie.js
@@ -120,6 +120,21 @@ const movies = [
     duration: "1h 28m",
   },
   {
+    title: "Burning Betrayal",
+    year: 2023,
+    rating: 4.0,
+    genre: "Drama/Mystery/Romance",
+    genres: ["drama", "mystery", "romance"],
+    country: "Spanyol",
+    countrys: ["spanyol"],
+    poster: "https://iili.io/JnCCjJj.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/fYQpOLrQ-BU",
+    synopsis:
+      "Babi discovers a betrayal by her long-term partner and decides to embark on a new adventure in life. On this journey, she meets judge Marco and they begin to live a story permeated by a lot of sexual tension.",
+    duration: "1h 38m",
+  },
+  {
     title: "Cabin Girl",
     year: 2023,
     rating: 4.6,
@@ -585,6 +600,21 @@ const movies = [
     duration: "1h 51m",
   },
   {
+    title: "Mission: Impossible - Dead Reckoning Part One",
+    year: 2023,
+    rating: 7.8,
+    genre: "Action/Adventure/Thriller",
+    genres: ["action", "adventure", "thriller"],
+    country: "USA",
+    countrys: ["usa"],
+    poster: "https://iili.io/JnCJnNn.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/avz06PDqDbM",
+    synopsis:
+      "Ethan Hunt and his IMF team must track down a dangerous weapon before it falls into the wrong hands.",
+    duration: "2h 46m",
+  },
+  {
     title: "Mother, May I?",
     year: 2023,
     rating: 5.1,
@@ -825,6 +855,21 @@ const movies = [
     duration: "1h 34m",
   },
   {
+    title: "Saw X",
+    year: 2023,
+    rating: 6.7,
+    genre: "Horror/Mystery/Thriller",
+    genres: ["horror", "mystery", "thriller"],
+    country: "USA",
+    countrys: ["usa"],
+    poster: "https://iili.io/JnC2VyP.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/t3PzUo4P21c",
+    synopsis:
+      "A sick and desperate John travels to Mexico for a risky and experimental medical procedure in hopes of a miracle cure for his cancer only to discover the entire operation is a scam to defraud the most vulnerable.",
+    duration: "1h 58m",
+  },
+  {
     title: "Soulmate",
     year: 2023,
     rating: 7.6,
@@ -990,6 +1035,21 @@ const movies = [
     duration: "1h 48m",
   },
   {
+    title: "The Killer",
+    year: 2023,
+    rating: 6.9,
+    genre: "Action/Adventure/Crime",
+    genres: ["action", "adventure", "crime"],
+    country: "USA",
+    countrys: ["usa"],
+    poster: "https://iili.io/JnCJVKQ.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/5S7FR_HCg9g",
+    synopsis:
+      "After a fateful near-miss, an assassin battles his employers and himself, on an international manhunt he insists isn't personal.",
+    duration: "1h 59m",
+  },
+  {
     title: "The Monkey King",
     year: 2023,
     rating: 5.8,
@@ -1123,6 +1183,21 @@ const movies = [
     synopsis:
       "During the '90s, a new faction of Transformers - the Maximals - join the Autobots as allies in the battle for Earth.",
     duration: "2h 7m",
+  },
+  {
+    title: "Under the Boardwalk",
+    year: 2023,
+    rating: 5.8,
+    genre: "Animation/Adventure/Comedy",
+    genres: ["animation", "adventure", "comedy"],
+    country: "USA",
+    countrys: ["usa"],
+    poster: "https://iili.io/JnCBhwN.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/vQmTVrNuxAI",
+    synopsis:
+      "Under the boardwalk at the Jersey Shore, a land crab townie falls in love with a sea crab tourist, but tensions between their friends and families rise. When a storm casts the duo far from home, their love will lead them on an adventure.",
+    duration: "1h 23m",
   },
   {
     title: "Unlocked",
@@ -4793,6 +4868,21 @@ const movies = [
     synopsis:
       "Vivo, Sony Pictures Animation's first-ever musical adventure featuring all-new original songs from Lin-Manuel Miranda, will take audiences on an epic adventure to gorgeous and vibrant locations never before seen in animation.",
     duration: "1h 35m",
+  },
+  {
+    title: "Wish Dragon",
+    year: 2021,
+    rating: 7.2,
+    genre: "Animation/Adventure/Comedy",
+    genres: ["action", "adventure", "comedy"],
+    country: "USA",
+    countrys: ["usa"],
+    poster: "https://iili.io/JnoRkLG.md.webp",
+    href: "./movie-gpt/detail.html",
+    embed: "https://www.youtube.com/embed/uWIRyU5fuzU",
+    synopsis:
+      "Determined teen Din is longing to reconnect with his childhood best friend when he meets a wish-granting dragon who shows him the magic of possibilities.",
+    duration: "1h 38m",
   },
   {
     title: "Wrath of Man",
